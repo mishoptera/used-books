4ae1466eb3247f4fdca0507db0e09f6bc5a262cd
Misha Leong <mishaleong@gmail.com>
2019-06-04 23:20:39 -0700

    some plans for how to proceed tomorrow

--- main.py
+++ main.py
@@ -7,3 +7,26 @@ Then build a feature list of book attributes to create a model that predicts boo
 
 
 """
+# get list of books for dataset
+def book_list():
+    """
+    Access goodreads listopia list of children's books and get all the associated tags and features
+    """
+    return list
+
+# Access lowest used book price from Amazon
+def get_used_price(isbn):
+
+    """
+    https://stackoverflow.com/questions/37638066/getting-lowest-used-price-with-amazon-api
+
+    price_response = amazon.ItemLookup(ItemId=isbn, IdType="ISBN", SearchIndex="Books", MerchantID="All", ResponseGroup="Small,OfferSummary")
+    desc_soup = BeautifulSoup(price_response) book_price = desc_soup.lowestusedprice.amount.get_text()
+    """
+    return ()
+
+# Access lowes new used book price from amazon
+def get_new_price(isbn):
+    """
+    This could be useful for sanity check and find weird outliers
+    """
