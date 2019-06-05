3f11b7312ee998055db8e0cdf5a40d9e02968e8e
Misha Leong <mishaleong@gmail.com>
2019-06-04 22:42:18 -0700

    gneral plan moving forward

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+The product is to build a model to allow users to take a photo of isbn of their children's book and determine a good price to set it at. Would be great to also take into account book quality and user preference for how quickly book should be sold to sell it. Maybe just give them a range of prices? Of low to high effort?
+
+Training data is to start with a list of children's books from goodreads. Then take the isbn for each of those books and get range of used book prices.
+
+Then build a feature list of book attributes to create a model that predicts book price.
+
+
+"""
