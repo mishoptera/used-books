2abcbeec0daeba1e89d1728131d931373e06dbca
Misha Leong <mishaleong@gmail.com>
2019-06-05 23:26:43 -0700

    figured out html codes that match up. Brand is not always present.

---
+++ html_detective.py
@@ -0,0 +1,57 @@
+# Will scrape sold versus unsold for the following information:
+# example from: view-source:
+#https://www.mercari.com/us/item/m14911119778/?ref=search_results and
+#https://www.mercari.com/us/item/m19778091444/?ref=search_results
+
+#ItemId
+-0.667162 12.622 0.306809Z" id="m14911119778" viewBox=
+-0.667162 12.622 0.306809Z" id="m19778091444" viewBox=
+
+
+# Date posted
+Posted</p></div><p class="Text__ProductText-sc-40whai-0 fndAKa Text2__Text2Normal-sc-1tydp9s-0 jEADfG Text__Text2-sc-1lvlnjo-3 btEJox Text-sc-1lvlnjo-0 hzcXJu">06/05/19</p>
+
+Posted</p></div><p class="Text__ProductText-sc-40whai-0 fndAKa Text2__Text2Normal-sc-1tydp9s-0 jEADfG Text__Text2-sc-1lvlnjo-3 btEJox Text-sc-1lvlnjo-0 hzcXJu">05/30/19</p>
+
+# Condition
+Condition</p><a class="Spec__FlexedAnchor-sc-1oxis5o-0 fERCrt Flex-ych44r-0-a ciOBDh" href="/help_center/article/316/"><svg class="withSvg__Svg-gnmida-0 cXNnqZ" width="12" height="12" style="margin-left:var(--space-1)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12"><path class="PathIcon-sc-1qr0hpz-1 kNnvTz PathIcon__Path-sc-1qr0hpz-0 hFTvWf" width="12" height="12" d="M6 0C9.3135 0 12 2.6865 12 6C12 9.3135 9.3135 12 6 12C2.6865 12 0 9.3135 0 6C0 2.6865 2.6865 0 6 0ZM4.3985 4.2215C4.542 3.371 5.3695 2.791 6.2495 2.933C7.1265 3.074 7.7165 3.8785 7.5735 4.7265C7.4835 5.059 7.2585 5.269 6.6685 5.6085L6.549 5.6765C5.631 6.2025 5.2535 6.611 5.2535 7.468H6.1645C6.1645 7.0375 6.342 6.8455 7.002 6.4675L7.122 6.3985C7.908 5.947 8.285 5.595 8.4635 4.9185C8.7 3.527 7.7665 2.2545 6.3945 2.0335C5.0245 1.8115 3.7275 2.7205 3.5 4.07L4.3985 4.2215ZM6.2275 9.7465V8.8355H5.3165V9.7465H6.2275Z" viewBox="0 0 12 12" fill="grey-3"></path></svg></a></div><p class="Text__ProductText-sc-40whai-0 fndAKa Text2__Text2Normal-sc-1tydp9s-0 jEADfG Text__Text2-sc-1lvlnjo-3 btEJox Text-sc-1lvlnjo-0 hzcXJu">Good</p>
+
+Condition</p><a class="Spec__FlexedAnchor-sc-1oxis5o-0 fERCrt Flex-ych44r-0-a ciOBDh" href="/help_center/article/316/"><svg class="withSvg__Svg-gnmida-0 cXNnqZ" width="12" height="12" style="margin-left:var(--space-1)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12"><path class="PathIcon-sc-1qr0hpz-1 kNnvTz PathIcon__Path-sc-1qr0hpz-0 hFTvWf" width="12" height="12" d="M6 0C9.3135 0 12 2.6865 12 6C12 9.3135 9.3135 12 6 12C2.6865 12 0 9.3135 0 6C0 2.6865 2.6865 0 6 0ZM4.3985 4.2215C4.542 3.371 5.3695 2.791 6.2495 2.933C7.1265 3.074 7.7165 3.8785 7.5735 4.7265C7.4835 5.059 7.2585 5.269 6.6685 5.6085L6.549 5.6765C5.631 6.2025 5.2535 6.611 5.2535 7.468H6.1645C6.1645 7.0375 6.342 6.8455 7.002 6.4675L7.122 6.3985C7.908 5.947 8.285 5.595 8.4635 4.9185C8.7 3.527 7.7665 2.2545 6.3945 2.0335C5.0245 1.8115 3.7275 2.7205 3.5 4.07L4.3985 4.2215ZM6.2275 9.7465V8.8355H5.3165V9.7465H6.2275Z" viewBox="0 0 12 12" fill="grey-3"></path></svg></a></div><p class="Text__ProductText-sc-40whai-0 fndAKa Text2__Text2Normal-sc-1tydp9s-0 jEADfG Text__Text2-sc-1lvlnjo-3 btEJox Text-sc-1lvlnjo-0 hzcXJu">Like new</p>
+
+# Shipping
+>Shipping</p><a class="Spec__FlexedAnchor-sc-1oxis5o-0 fERCrt Flex-ych44r-0-a ciOBDh" href="/help_center/article/315/"><svg class="withSvg__Svg-gnmida-0 cXNnqZ" width="12" height="12" style="margin-left:var(--space-1)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12"><path class="PathIcon-sc-1qr0hpz-1 kNnvTz PathIcon__Path-sc-1qr0hpz-0 hFTvWf" width="12" height="12" d="M6 0C9.3135 0 12 2.6865 12 6C12 9.3135 9.3135 12 6 12C2.6865 12 0 9.3135 0 6C0 2.6865 2.6865 0 6 0ZM4.3985 4.2215C4.542 3.371 5.3695 2.791 6.2495 2.933C7.1265 3.074 7.7165 3.8785 7.5735 4.7265C7.4835 5.059 7.2585 5.269 6.6685 5.6085L6.549 5.6765C5.631 6.2025 5.2535 6.611 5.2535 7.468H6.1645C6.1645 7.0375 6.342 6.8455 7.002 6.4675L7.122 6.3985C7.908 5.947 8.285 5.595 8.4635 4.9185C8.7 3.527 7.7665 2.2545 6.3945 2.0335C5.0245 1.8115 3.7275 2.7205 3.5 4.07L4.3985 4.2215ZM6.2275 9.7465V8.8355H5.3165V9.7465H6.2275Z" viewBox="0 0 12 12" fill="grey-3"></path></svg></a></div><p class="Text__ProductText-sc-40whai-0 fndAKa Text2__Text2Normal-sc-1tydp9s-0 jEADfG Text__Text2-sc-1lvlnjo-3 btEJox Text-sc-1lvlnjo-0 hzcXJu">Free | from Florida</p>
+
+>Shipping</p><a class="Spec__FlexedAnchor-sc-1oxis5o-0 fERCrt Flex-ych44r-0-a ciOBDh" href="/help_center/article/315/"><svg class="withSvg__Svg-gnmida-0 cXNnqZ" width="12" height="12" style="margin-left:var(--space-1)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12"><path class="PathIcon-sc-1qr0hpz-1 kNnvTz PathIcon__Path-sc-1qr0hpz-0 hFTvWf" width="12" height="12" d="M6 0C9.3135 0 12 2.6865 12 6C12 9.3135 9.3135 12 6 12C2.6865 12 0 9.3135 0 6C0 2.6865 2.6865 0 6 0ZM4.3985 4.2215C4.542 3.371 5.3695 2.791 6.2495 2.933C7.1265 3.074 7.7165 3.8785 7.5735 4.7265C7.4835 5.059 7.2585 5.269 6.6685 5.6085L6.549 5.6765C5.631 6.2025 5.2535 6.611 5.2535 7.468H6.1645C6.1645 7.0375 6.342 6.8455 7.002 6.4675L7.122 6.3985C7.908 5.947 8.285 5.595 8.4635 4.9185C8.7 3.527 7.7665 2.2545 6.3945 2.0335C5.0245 1.8115 3.7275 2.7205 3.5 4.07L4.3985 4.2215ZM6.2275 9.7465V8.8355H5.3165V9.7465H6.2275Z" viewBox="0 0 12 12" fill="grey-3"></path></svg></a></div><p class="Text__ProductText-sc-40whai-0 fndAKa Text2__Text2Normal-sc-1tydp9s-0 jEADfG Text__Text2-sc-1lvlnjo-3 btEJox Text-sc-1lvlnjo-0 hzcXJu">$11.50 | 3+ days | from Kansas</p>
+
+# Brand
+Brand</p></div><p class="Text__ProductText-sc-40whai-0 fndAKa Text2__Text2Normal-sc-1tydp9s-0 jEADfG Text__Text2-sc-1lvlnjo-3 btEJox Text-sc-1lvlnjo-0 hzcXJu"><a class="Link__StyledPlainLink-dkjuk2-2 fMkShC Link__StyledAnchor-dkjuk2-0 fWIppX Text__LinkText-sc-1e98qiv-0-a gtGdVC" href="/us/brand/5661/" style="position:relative">Scholastic</a></p>
+
+NO BRAND FOR SECOND LISTING (NA)
+
+# Category
+Category</p></div><p class="Text__ProductText-sc-40whai-0 fndAKa Text2__Text2Normal-sc-1tydp9s-0 jEADfG Text__Text2-sc-1lvlnjo-3 btEJox Text-sc-1lvlnjo-0 hzcXJu" style="white-space:pre-wrap"><a class="Link__StyledPlainLink-dkjuk2-2 fMkShC Link__StyledAnchor-dkjuk2-0 fWIppX Text__LinkText-sc-1e98qiv-0-a gtGdVC" href="/us/category/10/" style="position:relative">Other</a>,
+<a class="Link__StyledPlainLink-dkjuk2-2 fMkShC Link__StyledAnchor-dkjuk2-0 fWIppX Text__LinkText-sc-1e98qiv-0-a gtGdVC" href="/us/category/141/" style="position:relative">Books</a>,
+<a class="Link__StyledPlainLink-dkjuk2-2 fMkShC Link__StyledAnchor-dkjuk2-0 fWIppX Text__LinkText-sc-1e98qiv-0-a gtGdVC" href="/us/category/1487/" style="position:relative">Children&#x27;s books</a>,
+<a class="Link__StyledPlainLink-dkjuk2-2 fMkShC Link__StyledAnchor-dkjuk2-0 fWIppX Text__LinkText-sc-1e98qiv-0-a gtGdVC" href="/us/brand/5661/1487/" style="position:relative">Scholastic Children&#x27;s Books</a></p>
+
+Category</p></div><p class="Text__ProductText-sc-40whai-0 fndAKa Text2__Text2Normal-sc-1tydp9s-0 jEADfG Text__Text2-sc-1lvlnjo-3 btEJox Text-sc-1lvlnjo-0 hzcXJu" style="white-space:pre-wrap"><a class="Link__StyledPlainLink-dkjuk2-2 fMkShC Link__StyledAnchor-dkjuk2-0 fWIppX Text__LinkText-sc-1e98qiv-0-a gtGdVC" href="/us/category/10/" style="position:relative">Other</a>,
+<a class="Link__StyledPlainLink-dkjuk2-2 fMkShC Link__StyledAnchor-dkjuk2-0 fWIppX Text__LinkText-sc-1e98qiv-0-a gtGdVC" href="/us/category/141/" style="position:relative">Books</a>,
+<a class="Link__StyledPlainLink-dkjuk2-2 fMkShC Link__StyledAnchor-dkjuk2-0 fWIppX Text__LinkText-sc-1e98qiv-0-a gtGdVC" href="/us/category/1487/" style="position:relative">Children&#x27;s books</a></p></div>
+
+# Description
+Description</p><p class="ItemDescription__DescriptionText-sc-1w7qr5f-0 RRzr Text3__Text3Normal-nb7hff-0 cAxzZc Text__Text3-sc-1lvlnjo-4 eeJIlV Text-sc-1lvlnjo-0 iyThov" color="black">2 paperback picture books and one audio CD for the book Never, Ever Shout in a Zoo.</p></div>
+
+Description</p><p class="ItemDescription__DescriptionText-sc-1w7qr5f-0 RRzr Text3__Text3Normal-nb7hff-0 cAxzZc Text__Text3-sc-1lvlnjo-4 eeJIlV Text-sc-1lvlnjo-0 iyThov" color="black">Your Baby Can Read Complete Set EUC
+
+Bundle and save</p></div><div
+
+# Seller info
+.ratings":{"count":49,"average":5,"__typename":"Ratings"}
+
+.ratings":{"count":1161,"average":5,"__typename":"Ratings"}
+
+#************************************************************
+#************************************************************
+
+
+#
